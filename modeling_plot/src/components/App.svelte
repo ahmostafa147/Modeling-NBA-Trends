bdbd06b44e949d6fc73f6ca97df506a8a332bf6c
NathanYKo <knathan774@gmail.com>
2024-02-19 21:48:22 -0800

    added logos

--- modeling_plot/src/components/App.svelte
+++ modeling_plot/src/components/App.svelte
@@ -22,6 +22,43 @@
 
   let tooltip;
 
+  const teamLogos = {
+  "Atlanta Hawks": "modeling_plot/src/components/logos/atlanta hawks.svg",
+  "Boston Celtics": "https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg",
+  "Brooklyn Nets": 'https://cdn.nba.com/logos/nba/1610612751/primary/L/logo.svg',
+  "New York Knicks": 'https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg',
+  "Philadelphia 76ers": 'https://cdn.nba.com/logos/nba/1610612755/primary/L/logo.svg',
+  "Toronto Raptors": 'https://cdn.nba.com/logos/nba/1610612761/primary/L/logo.svg',
+  "Chicago Bulls": 'https://cdn.nba.com/logos/nba/1610612741/primary/L/logo.svg',
+  "Cleveland Cavaliers": 'https://cdn.nba.com/logos/nba/1610612739/primary/L/logo.svg',
+  "Detroit Pistons": 'https://cdn.nba.com/logos/nba/1610612765/primary/L/logo.svg',
+  "Indiana Pacers": 'https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg',
+  "Milwaukee Bucks": 'https://cdn.nba.com/logos/nba/1610612749/primary/L/logo.svg',
+  "Atlanta Hawks": 'https://cdn.nba.com/logos/nba/1610612737/primary/L/logo.svg',
+  "Charlotte Hornets": 'https://cdn.nba.com/logos/nba/1610612766/primary/L/logo.svg',
+  "Miami Heat": 'https://cdn.nba.com/logos/nba/1610612748/primary/L/logo.svg',
+  "Orlando Magic": 'https://cdn.nba.com/logos/nba/1610612753/primary/L/logo.svg' ,
+  "Washington Wizards": 'https://cdn.nba.com/logos/nba/1610612764/primary/L/logo.svg',
+  "Denver Nuggets": 'https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg',
+  "Minnesota Timberwolves": 'https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg',
+  "Oklahoma City Thunder": 'https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg',
+  "Portland Trail Blazers": 'https://cdn.nba.com/logos/nba/1610612757/primary/L/logo.svg',
+  "Utah Jazz": 'https://cdn.nba.com/logos/nba/1610612762/primary/L/logo.svg',
+  "Golden State Warriors": 'https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg',
+  "Los Angeles Clippers": 'https://cdn.nba.com/logos/nba/1610612746/primary/L/logo.svg',
+  "Los Angeles Lakers": 'https://cdn.nba.com/logos/nba/1610612747/primary/L/logo.svg',
+  "Phoenix Suns": 'https://cdn.nba.com/logos/nba/1610612756/primary/L/logo.svg',
+  "Sacramento Kings": 'https://cdn.nba.com/logos/nba/1610612758/primary/L/logo.svg',
+  "Dallas Mavericks": 'https://cdn.nba.com/logos/nba/1610612742/primary/L/logo.svg',
+  "Houston Rockets": 'https://cdn.nba.com/logos/nba/1610612745/primary/L/logo.svg',
+  "Memphis Grizzlies": 'https://cdn.nba.com/logos/nba/1610612763/primary/L/logo.svg',
+  "New Orleans Pelicans": 'https://cdn.nba.com/logos/nba/1610612740/primary/L/logo.svg',
+  "San Antonio Spurs": 'https://cdn.nba.com/logos/nba/1610612759/primary/L/logo.svg'
+
+
+
+};
+
   onMount(() => {
     d3.csv("src/DSC106_NBA.csv").then((csvData) => {
       data = csvData;
@@ -35,8 +72,11 @@
     values = Object.entries(teams[0])
       .filter((entry) => entry[0] !== "Year")
       .map(([key, value]) => {
-        return { team: key, score: value, logo: 'https://loodibee.com/wp-content/uploads/nba-atlanta-hawks-logo-480x480.png' };
-      });
+    const logo = teamLogos[key];
+      return { team: key, score: value, logo: logo || 'https://cdn.freebiesupply.com/images/large/2x/nba-logo-transparent.png' }; // Provide a default logo URL if not found
+    });
+
+
 
     const margin = { top: 20, right: 120, bottom: 100, left: 60 };
     const width = 1400 - margin.left - margin.right;
